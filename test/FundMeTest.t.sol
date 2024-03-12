55d848cceea52b7ae24da25855c37ab49f003111
Sebastian Contreras <sstfmscontreras@gmail.com>
2024-03-12 18:21:28 -0500

    added optimization of gas on withdraw function

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -111,6 +111,30 @@ contract FundMeTest is Test {
 
     }
 
+    function testWithdrawFromMultipleFundersCheaper() public funded {
+        // Arrange
+        uint160 numberOfFunders = 10;
+        uint160 startingFundersIndex = 2;
+        for(uint160 i = startingFundersIndex; i < numberOfFunders; i++) {
+            // vm.prank new address
+            // vm.deal new address
+            hoax(address(i), SEND_VALUE);
+            fundMe.fund{value: SEND_VALUE}();
+        }
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        // Act
+        vm.startPrank(fundMe.getOwner());
+        fundMe.cheaperWithdraw();
+        vm.stopPrank();
+
+        // Assert
+        assert(address(fundMe).balance == 0);
+        assert(startingFundMeBalance + startingOwnerBalance == fundMe.getOwner().balance);
+
+    }
+
     modifier funded() {
         vm.prank(USER);
         fundMe.fund{value: SEND_VALUE}();
